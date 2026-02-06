28301d6feb982432b596df31b743044d8a11eea3
Thomas Mol <email@thomasmol.com>
2026-02-06 13:31:02 +0100

    move auth and api

--- apps/auth/subjects.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-import { createSubjects } from "@openauthjs/openauth/subject"
-
-export const subjects = createSubjects({
-  user: z.object({
-    id: z.string(),
-  }),
-})
